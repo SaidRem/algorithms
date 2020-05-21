9e04d5fbc9697efb4ca85def2acbc8978a0e292d
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-05-21 20:54:21 +0400

    Add files via upload

---
+++ Merge_sort.py
@@ -0,0 +1,22 @@
+# Pseudocode of merge sort algorithm:
+# 
+# Merge(A, p, q, r)
+# 
+# 1. n1 = q - p + 1
+# 2. n2 = r - q
+# 3. Let be L[1...n1 + 1] and R[1...n2 + 1] - new arrays
+# 4. for i = 1 to n1
+# 5.      L[i] = A[p + i - 1]
+# 6. for j = 1 to n2
+# 7.      R[j] = A[q +j]
+# 8. L[n1 + 1] = infinity
+# 9. R[n2 + 1] = infinity
+# 10. i = 1
+# 11. j = 1
+# 12. for k = p to r
+# 13.     if L[i] <= R[j]
+# 14.             A[k] = L[i]
+# 15.             i = i + 1
+# 16.     else A[k] = R[j]
+# 17.             j = j + 1
+# 
