1ea22538d123a1e84fe5384b299e303ca9f87e34
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-05-22 21:19:27 +0400

    Add files via upload

---
+++ Dijkstras_algorithm.py
@@ -0,0 +1,11 @@
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors:
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
