858b2484df671182156d8a9684609b6a794310e0
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-05-29 21:54:30 +0400

    Add files via upload

--- QuickSort.py
+++ QuickSort.py
@@ -5,9 +5,10 @@ def quick_sort(arr):
     if len(arr) < 2:
         return arr
     else:
-        pivot = arr[0]
-        less = [x for x in arr[1:] if x <= pivot]
-        greater = [x for x in arr[1:] if x > pivot]
+        r = random.randint(0, len(arr) - 1)
+        pivot = arr.pop(r)
+        less = [x for x in arr if x <= pivot]
+        greater = [x for x in arr if x > pivot]
     return quick_sort(less) + [pivot] + quick_sort(greater)
 
 
