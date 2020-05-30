44494abdc351b12c5e0a2dd7935f60e692e4ca4d
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-05-30 21:50:41 +0400

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -6,7 +6,7 @@ def quick_sort(arr):
         return arr
     else:
         r = random.randint(0, len(arr) - 1)     # random int for select pivot element in random way
-        pivot = arr.pop(r)                      # random selection of pivot element is more effective
+        pivot = arr.pop(r)                      # random selection of pivot element makes average time = n*log(n)
         less = [x for x in arr if x <= pivot]
         greater = [x for x in arr if x > pivot]
     return quick_sort(less) + [pivot] + quick_sort(greater)
