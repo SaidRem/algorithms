4d204c21b8b3e7ab974ea38ca92e5f8c7bb7c11f
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-06-03 21:57:15 +0400

    Add files via upload

--- Random_Walk.py
+++ Random_Walk.py
@@ -1,5 +1,24 @@
 # Random walk
+import random
 
 
-def random_walk():
-    pass
+def random_walk(n):
+    """ Returns coordinates after n block of random walk """
+    x = 0
+    y = 0
+    for _ in range(n):
+        direct = ['N', 'S', 'W', 'E']
+        choice_r = random.choice(direct)
+        if choice_r == 'N':
+            y += 1
+        elif choice_r == 'S':
+            y -= 1
+        elif choice_r == 'W':
+            x -= 1
+        else:
+            x += 1
+    return x, y
+
+
+if __name__ == '__main__':
+    print(random_walk(19))
