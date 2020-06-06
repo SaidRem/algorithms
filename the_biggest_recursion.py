379c6cc779cd0e11ed7e8f1f29bc255c45155ebf
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-06-06 22:27:23 +0400

    Add files via upload

---
+++ the_biggest_recursion.py
@@ -0,0 +1,13 @@
+# the biggest with recursion
+
+
+def the_biggest(arr):
+    if len(arr) == 2:
+        return arr[0] if arr[0] > arr[1] else arr[1]
+    sub_max = the_biggest(arr[1:])
+    return arr[0] if arr[0] > sub_max else sub_max
+
+
+if __name__ == '__main__':
+    arr = list(map(int, input().strip().split()))
+    print(the_biggest(arr))
