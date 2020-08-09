1ce9f77f84ce7a39bcdc98ad3bf959b1d45bcd7a
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-08-09 21:31:56 +0400

    Update Binary_search.py

--- Binary_search.py
+++ Binary_search.py
@@ -1,3 +1,5 @@
+# Binary search, also known as half-interval search, logarithmic search, or binary chop is a search algorithm
+# that finds the position of a target value within a sorted array.
 def binary_search(arr, target):
     high = len(arr) - 1
     low = 0
