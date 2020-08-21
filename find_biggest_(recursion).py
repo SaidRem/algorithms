25a56394d64efff85aaea453d725555cab0cf135
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-08-21 23:50:10 +0400

    Update find_biggest_(recursion).py

--- find_biggest_(recursion).py
+++ find_biggest_(recursion).py
@@ -2,8 +2,10 @@
 
 
 def the_biggest(arr):
+    # Base case if length of array equals 2 .
     if len(arr) == 2:
         return arr[0] if arr[0] > arr[1] else arr[1]
+    # Recursion case.
     sub_max = the_biggest(arr[1:])
     return arr[0] if arr[0] > sub_max else sub_max
 
